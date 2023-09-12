2f5c73282c994aa1ed86a11acc34924dc5325d73
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-09-12 15:55:21 +0200

    Writing API's

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const fs = require("fs");
+const session = require("express-session");
 
 const app = express();
 const port = 6000;
 
 app.use(express.json());
 
+app.use(session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 
-app.get("/", async(req, res) => {
+const isAuth = (req, res, next) => {
+    if (req.session.username) {
+        next();
+    } else {
+        res.send("Not Authenticated");
+    }
+}
+
+app.get("/", isAuth, async(req, res) => {
     const quiz = await JSON.parse(fs.readFileSync("./data/quiz.json"))
     res.send(quiz);
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const users = await JSON.parse(fs.readFileSync("./data/users.json"));
+
+    users.push({username, password}).then(() => {
+        res.send("User registered");
+    }).catch((err) => {
+        res.send("Error registering user");
+    })
+});
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
@@ -23,7 +49,8 @@ app.post("/login", async (req, res) => {
     .find((user) => user.username === username && user.password === password)
     .then((result) => {
       if (result) {
-        res.send("Login Success");
+        req.session.username = username;
+        res.send(username);
       } else {
         res.send("Login Failed");
       }
@@ -33,6 +60,9 @@ app.post("/login", async (req, res) => {
     });
 });
 
-app.get("/questions", async (req, res) => {});
+app.get("/questions", isAuth, async (req, res) => {
+    const questions = await JSON.parse(fs.readFileSync("./data/questions.json"));
+    res.send(questions);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
