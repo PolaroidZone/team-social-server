2e6558c2581a02e46d767c6b9785a94f76d500d7
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-09-19 12:06:28 +0200

    Completed server apis

--- index.js
+++ index.js
@@ -1,68 +1,60 @@
 const express = require("express");
+const cors = require("cors");
+const scrapeMemes = require("./puppet.js");
+
 const fs = require("fs");
-const session = require("express-session");
+
+const port = 3030;
 
 const app = express();
-const port = 6000;
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.use(session({
-    secret: "secret-key",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}))
+app.get("/", async (_req, res) => {
+  // Search phrases
+  try {
+    const bad = await scrapeMemes("bad");
+    const good = await scrapeMemes("good");
+    const amazing = await scrapeMemes("amazing");
 
-const isAuth = (req, res, next) => {
-    if (req.session.username) {
-        next();
-    } else {
-        res.send("Not Authenticated");
-    }
-}
+    // Create an array of meme objects
+    const memesArray = {
+      memes: [
+        { category: "bad", imageUrl: bad },
+        { category: "good", imageUrl: good },
+        { category: "amazing", imageUrl: amazing },
+      ],
+    };
 
-app.get("/", isAuth, async(req, res) => {
-    const quiz = await JSON.parse(fs.readFileSync("./data/quiz.json"))
-    res.send(quiz);
-});
-
-app.post("/register", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const users = await JSON.parse(fs.readFileSync("./data/users.json"));
-
-    users.push({username, password}).then(() => {
-        res.send("User registered");
-    }).catch((err) => {
-        res.send("Error registering user");
-    })
-});
-
-app.post("/login", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const users = JSON.parse(fs.readFileSync("./data/users.json"));
-
-  users
-    .find((user) => user.username === username && user.password === password)
-    .then((result) => {
-      if (result) {
-        req.session.username = username;
-        res.send(username);
-      } else {
-        res.send("Login Failed");
-      }
-    })
-    .catch((err) => {
-        res.send("User not found");
+    fs.writeFile("memes.json", JSON.stringify(memesArray), (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
     });
+
+    res.status(200).json(memesArray);
+  } catch (error) {
+    console.error("Error:", error);
+    //error code
+    res.status(500).send("An error occurred");
+  }
 });
 
-app.get("/questions", isAuth, async (req, res) => {
-    const questions = await JSON.parse(fs.readFileSync("./data/questions.json"));
-    res.send(questions);
+app.get("/memes", async (_req, res) => {
+  try {
+    const memes = fs.readFileSync("memes.json");
+    res.status(200).json(JSON.parse(memes));
+  } catch (error) {
+    console.error("Error:", error);
+    //error code
+    res.status(500).send("An error occurred");
+  }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Example app listening on http://localhost:${port}`)
+);
